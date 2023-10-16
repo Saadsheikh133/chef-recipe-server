7df21e74e46bc78d12cba3141fbdd91644b7eb4c
saadsheikh133 <mdsaadsheikh7096@gmail.com>
2023-10-17 03:59:37 +0600

    added foods api and implement get method on foods.json and sent the data to the client site

--- index.js
+++ index.js
@@ -6,21 +6,26 @@ const cors = require("cors");
 app.use(cors());
 
 const chefInfo = require('./data/chefInfo.json');
+const foods = require('./data/foods.json');
 
 app.get('/', (req, res) => {
     res.send('api comming soon')
-})
+});
 
 app.get('/chefInfo/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const selectedChef = chefInfo.find(info => info.id === id);
     res.send(selectedChef)
-})
+});
 
 app.get('/chefInfo', (req, res) => {
     res.send(chefInfo);
-})
+});
+
+app.get("/foods", (req, res) => {
+    res.send(foods);
+});
 
 app.listen(port, () => {
     console.log('hello world')
-})
+});
